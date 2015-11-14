96ca61f26c174a675e9192c55e617a0fa1fd9d50
Qiming Weng <qimingweng@users.noreply.github.com>
2015-11-14 00:10:29 -0500

    linting

--- site/src/js/components/HomePageExampleModalButton.react.js
+++ site/src/js/components/HomePageExampleModalButton.react.js
@@ -4,83 +4,80 @@ import {ModalContainer, ModalDialog} from 'react-modal-dialog';
 import ReactSpinner from 'react-spinjs';
 
 export default class HomePageExampleModalButton extends React.Component {
+  static propTypes = {
+    className: PropTypes.string,
+  }
   state = {
-    showModal: false
+    showModal: false,
   }
   openModal = () => {
-    this.setState({showModal: true})
+    this.setState({showModal: true});
   }
   closeModal = () => {
-    this.setState({showModal: false})
+    this.setState({showModal: false});
   }
   render() {
-    return (
-      <a className={this.props.className} onClick={this.openModal}>
-        Open A Modal
-        {this.state.showModal ?
-          <FirstModal onClose={this.closeModal}/>
-        : null}
-      </a>
-    )
+    return <a className={this.props.className} onClick={this.openModal}>
+      Open A Modal
+      {this.state.showModal ?
+        <FirstModal onClose={this.closeModal}/>
+      : null}
+    </a>;
   }
 }
 
 class FirstModal extends React.Component {
   static propTypes = {
-    onClose: PropTypes.func
+    onClose: PropTypes.func,
   }
   state = {
     showSecondModal: false,
-    isLoading: false
+    isLoading: false,
   }
   openModal = () => {
-    this.setState({showSecondModal: true})
+    this.setState({showSecondModal: true});
   }
   closeModal = () => {
-    this.setState({showSecondModal: false})
+    this.setState({showSecondModal: false});
   }
   load = () => {
-    this.setState({isLoading: true})
+    this.setState({isLoading: true});
 
     setTimeout(() => {
       this.setState({isLoading: false});
     }, 500);
   }
   render() {
-    return (
-      <ModalContainer onClose={this.props.onClose}>
-        {this.state.isLoading ?
-          <ReactSpinner color="white"/>
-          :
-          <ModalDialog onClose={this.props.onClose} className="example-dialog">
-            {this.state.showSecondModal ?
-              <SecondModal onClose={this.closeModal}/>
-            : null}
+    return <ModalContainer onClose={this.props.onClose}>
+      {this.state.isLoading ?
+        <ReactSpinner color="white"/>
+        :
+        <ModalDialog onClose={this.props.onClose} className="example-dialog">
+          {this.state.showSecondModal ?
+            <SecondModal onClose={this.closeModal}/>
+          : null}
 
-            <h1>This is a Modal Dialog</h1>
-            <p>You can hit esc to close it, or click outside the boundaries</p>
-            <p>You can also click the close button</p>
-            <p>You can open up a <a onClick={this.openModal}>second modal</a> within this!</p>
-            <p>You can make this modal <a onClick={this.load}>load</a></p>
-          </ModalDialog>
-        }
-      </ModalContainer>
-    )
+          <h1>This is a Modal Dialog</h1>
+          <p>You can hit esc to close it, or click outside the boundaries</p>
+          <p>You can also click the close button</p>
+          <p>You can open up a <a onClick={this.openModal}>second modal</a> within this!</p>
+          <p>You can make this modal <a onClick={this.load}>load</a></p>
+        </ModalDialog>
+      }
+    </ModalContainer>;
   }
 }
 
 class SecondModal extends React.Component {
   static propTypes = {
-    onClose: PropTypes.func
+    onClose: PropTypes.func,
   }
   render() {
-    return (
-      <ModalContainer onClose={this.props.onClose}>
-        <ModalDialog onClose={this.props.onClose} width={350} className="example-dialog">
-          <h1>Second Dialog</h1>
-          <p>When you hit esc, only this one will close</p>
-        </ModalDialog>
-      </ModalContainer>
-    )
+    return <ModalContainer onClose={this.props.onClose}>
+      <ModalDialog onClose={this.props.onClose} width={350} className="example-dialog">
+        <h1>Second Dialog</h1>
+        <p>When you hit esc, only this one will close</p>
+      </ModalDialog>
+    </ModalContainer>;
   }
 }
--- site/src/js/pages/HomePage.js
+++ site/src/js/pages/HomePage.js
@@ -9,25 +9,23 @@ require('../../css/HomePage.scss');
 
 export default class HomePage extends React.Component {
   render() {
-    return (
-      <div id="home-page">
-        <header>
-          <Container>
-            <h1>React Modal Dialog</h1>
-            <h2>A simple, idiomatic, and declarative way to launch modal dialogs in ReactJS</h2>
+    return <div id="home-page">
+      <header>
+        <Container>
+          <h1>React Modal Dialog</h1>
+          <h2>A simple, idiomatic, and declarative way to launch modal dialogs in ReactJS</h2>
 
-            <HomePageExampleModalButton className="header-link"/>
-            <a className="header-link" href="https://github.com/qimingweng/react-modal-dialog">Github</a>
-          </Container>
-        </header>
+          <HomePageExampleModalButton className="header-link"/>
+          <a className="header-link" href="https://github.com/qimingweng/react-modal-dialog">Github</a>
+        </Container>
+      </header>
 
-        <section className="body-section">
-          <Container>
-            <HomeContent/>
-          </Container>
-        </section>
-      </div>
-    )
+      <section className="body-section">
+        <Container>
+          <HomeContent/>
+        </Container>
+      </section>
+    </div>;
   }
 }
 
@@ -35,20 +33,19 @@ const markdown = require('raw!../markdown/HomePage.md');
 
 class HomeContent extends React.Component {
   render() {
-    return (
-      <div dangerouslySetInnerHTML={{
-        __html: marked.parse(markdown, {renderer: customMarkedRenderer})
-      }}/>
-    )
+    return <div dangerouslySetInnerHTML={{
+      __html: marked.parse(markdown, {renderer: customMarkedRenderer}),
+    }}/>;
   }
 }
 
 class Container extends React.Component {
+  static propTypes = {
+    children: PropTypes.node,
+  }
   render() {
-    return (
-      <div style={{width: 600, margin: '0 auto'}}>
-        {this.props.children}
-      </div>
-    )
+    return <div style={{width: 600, margin: '0 auto'}}>
+      {this.props.children}
+    </div>;
   }
 }

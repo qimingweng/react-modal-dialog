2a4d18eb47f22251c9c6e2aa6570ce8608753dfa
bluejamesbond <MathewKurian@utexas.edu>
2016-11-12 09:48:36 -0800

    Updating dependencies

--- src/CloseCircle.js
+++ src/CloseCircle.js
@@ -4,33 +4,35 @@ import React, {PropTypes} from 'react';
 const RECT_WIDTH = 1.5;
 const MARGIN = 8;
 
-const CloseCircle = (props) => {
-  const {
-    diameter,
-  } = props;
+class CloseCircle extends React.PureComponent {
+  render() {
+    const {
+      diameter,
+    } = this.props;
 
-  const radius = diameter / 2;
+    const radius = diameter / 2;
 
-  return <svg width={diameter} height={diameter}>
-    <circle cx={radius} cy={radius} r={radius} fill="black"/>
-    <g transform={`rotate(45 ${diameter / 2} ${diameter / 2})`}>
-      <rect
-        x={MARGIN}
-        y={(diameter - RECT_WIDTH) / 2}
-        width={diameter - (2 * MARGIN)}
-        height={RECT_WIDTH}
-        fill="white"
-      />
-      <rect
-        y={MARGIN}
-        x={(diameter - RECT_WIDTH) / 2}
-        height={diameter - (2 * MARGIN)}
-        width={RECT_WIDTH}
-        fill="white"
-      />
-    </g>
-  </svg>;
-};
+    return <svg width={diameter} height={diameter}>
+      <circle cx={radius} cy={radius} r={radius} fill="black"/>
+      <g transform={`rotate(45 ${diameter / 2} ${diameter / 2})`}>
+        <rect
+          x={MARGIN}
+          y={(diameter - RECT_WIDTH) / 2}
+          width={diameter - (2 * MARGIN)}
+          height={RECT_WIDTH}
+          fill="white"
+        />
+        <rect
+          y={MARGIN}
+          x={(diameter - RECT_WIDTH) / 2}
+          height={diameter - (2 * MARGIN)}
+          width={RECT_WIDTH}
+          fill="white"
+        />
+      </g>
+    </svg>;
+  }
+}
 
 CloseCircle.propTypes = {
   diameter: PropTypes.number.isRequired,

b5877a04bd37a9d20478794246d591296aa025e9
edrpls <edr@edrpls.com>
2017-02-07 11:46:10 -0600

    Improvements in config

--- webpack.build.js
+++ webpack.build.js
@@ -2,38 +2,14 @@ const webpack = require('webpack');
 
 module.exports = {
 
+    cache: false,
+
+    entry: './src/index.js',
     output: {
         path: 'lib/',
         publicPath: '/',
         filename: 'index.js'
     },
-
-    cache: false,
-
-    entry: './src/index.js',
-
-    //node: {
-    //    net: 'empty',
-    //    dns: 'empty'
-    //},
-
-    stats: false,
-
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader',
-            options: {
-                presets: ['react', 'stage-0'],
-                plugins: ['lodash', [
-                    'transform-runtime',
-                    {'polyfill': false}
-                ]]
-            }
-        }]
-    },
-
     plugins: [
         new webpack.LoaderOptionsPlugin({debug: true}),
         new webpack.DefinePlugin({'process.env': {'NODE_ENV': '"production"'}}),
@@ -45,7 +21,23 @@ module.exports = {
                 comments: false
             }
         }),
-        new webpack.optimize.AggressiveMergingPlugin(),
-        new webpack.NoErrorsPlugin()
-    ]
+        new webpack.optimize.AggressiveMergingPlugin()
+    ],
+
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['react', 'stage-0'],
+                plugins: [
+                    'lodash',
+                    'transform-decorators-legacy',
+                    'transform-runtime'
+                ]
+            }
+        }]
+    }
+
 };

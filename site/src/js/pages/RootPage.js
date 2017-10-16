7e7cec8df8e9b7e4b2c8a12dc028eb3fb377da6f
Sandro Machado <sandro@uphold.com>
2017-10-16 16:06:38 +0100

    Bump to React 16

--- site/src/js/pages/RootPage.js
+++ site/src/js/pages/RootPage.js
@@ -1,5 +1,6 @@
 // RootPage
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import {RouteHandler} from 'react-router';
 
 require('../../css/RootPage.scss');
--- src/ModalContainer.js
+++ src/ModalContainer.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import ModalPortal from './ModalPortal';
 import ModalBackground from './ModalBackground';
